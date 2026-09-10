1cccea0c680b451c3deb7dd327d9e3d11f7ca178
ginanjargigin <ginanjargigin33@gmail.com>
2026-09-10 16:05:42 +0700

    Add default sheets data structure

---
+++ src/data/defaultSheets.js
@@ -0,0 +1,92 @@
+import { uid } from "../utils/appUtils";
+
+export const DEFAULT_SHEETS = [
+  {
+    id: uid(),
+    name: "PSV 60",
+    metrics: [{ id: uid(), name: "Std", ct: 13.4 }],
+  },
+  {
+    id: uid(),
+    name: "PSV Bintang 60",
+    metrics: [{ id: uid(), name: "Std", ct: 13.4 }],
+  },
+  {
+    id: uid(),
+    name: "Ballguide Merah",
+    metrics: [{ id: uid(), name: "Std", ct: 2.64 }],
+  },
+  {
+    id: uid(),
+    name: "Ballguide Putih",
+    metrics: [{ id: uid(), name: "Std", ct: 2.64 }],
+  },
+  {
+    id: uid(),
+    name: "Reinforce Merah",
+    metrics: [{ id: uid(), name: "Std", ct: 14.4 }],
+  },
+  {
+    id: uid(),
+    name: "Reinforce Putih",
+    metrics: [{ id: uid(), name: "Std", ct: 14.4 }],
+  },
+  {
+    id: uid(),
+    name: "Machining Merah",
+    metrics: [
+      { id: uid(), name: "LA", ct: 15 },
+      { id: uid(), name: "LA14", ct: 25.8 },
+      { id: uid(), name: "RT", ct: 13.8 },
+    ],
+  },
+  {
+    id: uid(),
+    name: "Machining Putih",
+    metrics: [
+      { id: uid(), name: "LA", ct: 15 },
+      { id: uid(), name: "LA14", ct: 25.8 },
+      { id: uid(), name: "RT", ct: 13.8 },
+    ],
+  },
+  {
+    id: uid(),
+    name: "YTB Staking Ari",
+    metrics: [{ id: uid(), name: "Std", ct: 49.2 }],
+  },
+  {
+    id: uid(),
+    name: "Passthrough 4L45W",
+    metrics: [{ id: uid(), name: "Std", ct: 9.6 }],
+  },
+  {
+    id: uid(),
+    name: "Passthrough 4L45W Merah",
+    metrics: [{ id: uid(), name: "Std", ct: 9.6 }],
+  },
+  {
+    id: uid(),
+    name: "T862",
+    metrics: [{ id: uid(), name: "Std", ct: 18.72 }],
+  },
+  {
+    id: uid(),
+    name: "Lever Staking",
+    metrics: [{ id: uid(), name: "Std", ct: 14.4 }],
+  },
+  {
+    id: uid(),
+    name: "TSAA",
+    metrics: [{ id: uid(), name: "Std", ct: 96 }],
+  },
+  {
+    id: uid(),
+    name: "DO1N 1",
+    metrics: [{ id: uid(), name: "Std", ct: 17.4 }],
+  },
+  {
+    id: uid(),
+    name: "DO1N 2",
+    metrics: [{ id: uid(), name: "Std", ct: 17.4 }],
+  },
+];
