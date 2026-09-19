b74e30dd30c47fadbabaf7eeff6b058ee24cb732
ginanjargigin <ginanjargigin33@gmail.com>
2026-09-19 15:43:08 +0700

    Refactor useAppActions to include new utility functions

--- src/hooks/useAppActions.js
+++ src/hooks/useAppActions.js
@@ -1,169 +1,306 @@
-import { clampInt } from "./appUtils";
+import {
+  createSheet,
+  addSheetToDb,
+  removeSheetFromDb,
+  updateSheetNameInDb,
+  addMetricToDb,
+  updateMetricInDb,
+  removeMetricFromDb,
+  moveSheetInDb,
+} from "../utils/sheetUtils";
 
-export function updateEntryInDb(
-  db,
-  mk,
+import {
+  updateEntryInDb,
+  updateNgEntryInDb,
+  updateNoteInDb,
+  clearEntryInDb,
+} from "../utils/monthDataUtils";
+
+import {
+  addNgTypeToDb,
+  updateNgTypeInDb,
+  removeNgTypeFromDb,
+} from "../utils/ngUtils";
+
+export function useAppActions({
+  setDb,
+  setSheetId,
   sheetId,
-  date,
-  metricId,
-  field,
-  raw
-) {
-  const val = clampInt(raw);
-
-  const next = {
-    ...db,
-    months: {
-      ...db.months,
-    },
-  };
-
-  const monthObj = {
-    ...(next.months[mk] || {}),
-  };
-
-  monthObj[sheetId] = {
-    ...(monthObj[sheetId] || {}),
-  };
-
-  monthObj[sheetId][date] = {
-    ...(monthObj[sheetId][date] || {}),
-  };
-
-  monthObj[sheetId][date][metricId] = {
-    ...(monthObj[sheetId][date][metricId] || {}),
-    [field]: val,
-  };
-
-  next.months[mk] = monthObj;
-
-  return next;
-}
-
-export function updateNgEntryInDb(
-  db,
   mk,
-  sheetId,
-  date,
-  ngTypeId,
-  raw
-) {
-  const val = clampInt(raw);
+  scheduleSave,
+}) {
+  const addNgType = (sId, name) => {
+    setDb((prev) => {
+      const next = addNgTypeToDb(
+        prev,
+        sId,
+        name
+      );
 
-  const next = {
-    ...db,
-    months: {
-      ...db.months,
-    },
+      scheduleSave(next);
+
+      return next;
+    });
   };
 
-  const monthObj = {
-    ...(next.months[mk] || {}),
+  const updateNgType = (
+    sId,
+    ngTypeId,
+    name
+  ) => {
+    setDb((prev) => {
+      const next = updateNgTypeInDb(
+        prev,
+        sId,
+        ngTypeId,
+        name
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
   };
 
-  monthObj[sheetId] = {
-    ...(monthObj[sheetId] || {}),
+  const removeNgType = (
+    sId,
+    ngTypeId
+  ) => {
+    setDb((prev) => {
+      const next = removeNgTypeFromDb(
+        prev,
+        sId,
+        ngTypeId
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
   };
 
-  monthObj[sheetId][date] = {
-    ...(monthObj[sheetId][date] || {}),
+  const updateEntry = (
+    sId,
+    d,
+    metricId,
+    field,
+    raw
+  ) => {
+    setDb((prev) => {
+      const next = updateEntryInDb(
+        prev,
+        mk,
+        sId,
+        d,
+        metricId,
+        field,
+        raw
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
   };
 
-  const dayObj = monthObj[sheetId][date];
+  const updateNgEntry = (
+    sId,
+    d,
+    ngTypeId,
+    raw
+  ) => {
+    setDb((prev) => {
+      const next = updateNgEntryInDb(
+        prev,
+        mk,
+        sId,
+        d,
+        ngTypeId,
+        raw
+      );
 
-  const currentNg = {
-    ...(dayObj.ng || {}),
+      scheduleSave(next);
+
+      return next;
+    });
   };
 
-  if (val === 0) {
-    delete currentNg[ngTypeId];
-  } else {
-    currentNg[ngTypeId] = val;
-  }
+  const updateNote = (
+    sId,
+    d,
+    note
+  ) => {
+    setDb((prev) => {
+      const next = updateNoteInDb(
+        prev,
+        mk,
+        sId,
+        d,
+        note
+      );
 
-  if (Object.keys(currentNg).length === 0) {
-    delete dayObj.ng;
-  } else {
-    dayObj.ng = currentNg;
-  }
+      scheduleSave(next);
 
-  monthObj[sheetId][date] = dayObj;
-  next.months[mk] = monthObj;
-
-  return next;
-}
-
-export function updateNoteInDb(
-  db,
-  mk,
-  sheetId,
-  date,
-  note
-) {
-  const next = {
-    ...db,
-    months: {
-      ...db.months,
-    },
+      return next;
+    });
   };
 
-  const monthObj = {
-    ...(next.months[mk] || {}),
+  const clearEntry = (
+    sId,
+    d
+  ) => {
+    setDb((prev) => {
+      const next = clearEntryInDb(
+        prev,
+        mk,
+        sId,
+        d
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
   };
 
-  monthObj[sheetId] = {
-    ...(monthObj[sheetId] || {}),
+  const addSheet = (name) => {
+    const s = createSheet(name);
+
+    setDb((prev) => {
+      const next = addSheetToDb(
+        prev,
+        s
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
+
+    setSheetId(s.id);
   };
 
-  const dayObj = {
-    ...(monthObj[sheetId][date] || {}),
+  const removeSheet = (sId) => {
+    setDb((prev) => {
+      const next = removeSheetFromDb(
+        prev,
+        sId
+      );
+
+      scheduleSave(next);
+
+      if (
+        sheetId === sId &&
+        next.sheets.length
+      ) {
+        setSheetId(
+          next.sheets[0].id
+        );
+      }
+
+      return next;
+    });
   };
 
-  const trimmedNote = String(note ?? "");
+  const updateSheetName = (
+    sId,
+    name
+  ) => {
+    setDb((prev) => {
+      const next = updateSheetNameInDb(
+        prev,
+        sId,
+        name
+      );
 
-  if (trimmedNote.trim() === "") {
-    delete dayObj.note;
-  } else {
-    dayObj.note = trimmedNote;
-  }
+      scheduleSave(next);
 
-  if (Object.keys(dayObj).length === 0) {
-    delete monthObj[sheetId][date];
-  } else {
-    monthObj[sheetId][date] = dayObj;
-  }
-
-  next.months[mk] = monthObj;
-
-  return next;
-}
-
-export function clearEntryInDb(
-  db,
-  mk,
-  sheetId,
-  date
-) {
-  const next = {
-    ...db,
-    months: {
-      ...db.months,
-    },
+      return next;
+    });
   };
 
-  const monthObj = {
-    ...(next.months[mk] || {}),
+  const addMetric = (sId) => {
+    setDb((prev) => {
+      const next = addMetricToDb(
+        prev,
+        sId
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
   };
 
-  if (monthObj[sheetId]) {
-    monthObj[sheetId] = {
-      ...monthObj[sheetId],
-    };
+  const updateMetric = (
+    sId,
+    mId,
+    field,
+    value
+  ) => {
+    setDb((prev) => {
+      const next = updateMetricInDb(
+        prev,
+        sId,
+        mId,
+        field,
+        value
+      );
 
-    delete monthObj[sheetId][date];
-  }
+      scheduleSave(next);
 
-  next.months[mk] = monthObj;
+      return next;
+    });
+  };
 
-  return next;
+  const removeMetric = (
+    sId,
+    mId
+  ) => {
+    setDb((prev) => {
+      const next = removeMetricFromDb(
+        prev,
+        sId,
+        mId
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
+  };
+
+  const moveSheet = (
+    sId,
+    direction
+  ) => {
+    setDb((prev) => {
+      const next = moveSheetInDb(
+        prev,
+        sId,
+        direction
+      );
+
+      scheduleSave(next);
+
+      return next;
+    });
+  };
+
+  return {
+    updateEntry,
+    updateNgEntry,
+    updateNote,
+    clearEntry,
+    addSheet,
+    removeSheet,
+    updateSheetName,
+    addMetric,
+    updateMetric,
+    removeMetric,
+    moveSheet,
+    addNgType,
+    updateNgType,
+    removeNgType,
+  };
 }
